66f1c96172292d0fdc95c1955054be2ca13e6f4c
Vishal Parameswaran <vishal@parameswaran.net>
2017-06-06 16:33:02 +0530

    Final Test v1

--- js/app.js
+++ js/app.js
@@ -1,1 +1,34 @@
-$(document).foundation()
+var sections = $('.row')
+    , nav = $('#mynavbar')
+    , nav_height = nav.outerHeight();
+
+$(window).on('scroll', function () {
+    var cur_pos = $(this).scrollTop();
+
+    sections.each(function () {
+        var top = $(this).offset().top - nav_height - $('.About').outerHeight(),
+            bottom = top + $(this).outerHeight();
+
+        if (cur_pos >= top && cur_pos <= bottom) {
+            nav.find('a').removeClass('active');
+            sections.removeClass('active');
+
+            $(this).addClass('active');
+            nav.find('a[href="#' + $(this).attr('id') + '"]').addClass('active');
+        }
+    });
+});
+$(function () {
+    var navoffset = $("#mynavbar").offset().top;
+    $('#mynavbar').wrap('<div class="nav-placeholder"></div>');
+    $('.nav-placeholder').height($('#mynavbar').outerHeight());
+    $(window).scroll(function () {
+        var scrollpos = $(window).scrollTop();
+        if (scrollpos >= navoffset) {
+            $('#mynavbar').addClass("fixed");
+        }
+        else {
+            $("#mynavbar").removeClass("fixed");
+        }
+    })
+});
--- js/inspiration.js
+++ js/inspiration.js
@@ -1,8 +1,9 @@
 var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'js/inspiration.json');
+ourRequest.open('GET', 'https://gist.githubusercontent.com/sausagenutter/908c8339b1d78c2da8bc1ae293e62a53/raw');
 ourRequest.onload = function() {
   if (ourRequest.status >= 200 && ourRequest.status < 400) {
     var data = JSON.parse(ourRequest.responseText);
+    console.log(data);
     createHTML(data);
   } else {
     console.log("We connected to the server, but it returned an error.");
@@ -16,8 +17,7 @@ ourRequest.send();
 function createHTML(workshopData) {
   var rawTemplate = document.getElementById("workshopTemplate").innerHTML;
   var compiledTemplate = Handlebars.compile(rawTemplate);
-  console.log(compiledTemplate);
   var ourGeneratedHTML = compiledTemplate(workshopData);
-  var workshopContainer = document.getElementById("events");
-  workshopContainer.innerHTML = ourGeneratedHTML;
+  var petsContainer = document.getElementById("events");
+  petsContainer.innerHTML = ourGeneratedHTML;
 }
